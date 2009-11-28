897739c3eb6c602031d4f7c60b7b19fbe63e2294
ethanmiller <thnntn@gmail.com>
2009-11-27 21:53:20 -0800

    got two windows, few other refinements

--- mod94.c
+++ mod94.c
@@ -2,22 +2,26 @@
 #include <sys/time.h>
 #include <signal.h>
 
-void hup(int);
+#define WINDIV 30
+#define INTRVL 41666 // approx 24 times a second
+
+void draws(int);
+int handle_char(char in_char);
 
 int main(){
     char ch;
-    init();
+    setup();
     while(1){
 	ch = getch();
-	printw("%c", ch);
-	refresh(); // Print it on to the real screen
+	handle_char(ch);
     }
     endwin();
     return 0;
+    // for scrolling see http://www.mkssoftware.com/docs/man3/curs_scroll.3.asp
 }
 
-int init(){
-    signal(SIGALRM, hup);
+int setup(){
+    signal(SIGALRM, draws);
     start_anim();
     initscr(); // Start curses mode
     cbreak();  // Line buffering disabled
@@ -25,18 +29,32 @@ int init(){
     return 0;
 }
 
+int handle_char(char in_char){
+    static WINDOW *charwin = NULL;
+    if(charwin == NULL){
+	charwin = newwin(LINES, WINDIV, 0, 0);
+    }
+    wprintw(charwin, "%c", in_char);
+    wrefresh(charwin);
+    return 0;
+}
+
 int start_anim(unsigned int seconds){
     struct itimerval ival;
-    ival.it_interval.tv_usec = 0;
-    ival.it_interval.tv_sec = 5;
-    ival.it_value.tv_usec = 0;
-    ival.it_value.tv_sec = 5;
+    ival.it_interval.tv_usec = INTRVL;
+    ival.it_interval.tv_sec = 0;
+    ival.it_value.tv_usec = INTRVL;
+    ival.it_value.tv_sec = 0;
     setitimer(ITIMER_REAL, &ival, NULL);
     return 0;
 }
 
-void hup(int sig){
-    printw(" hup! ");
-    refresh();
-    signal(sig, hup);
+void draws(int sig){
+    static WINDOW *vizwin = NULL;
+    if(vizwin == NULL){
+	vizwin = newwin(LINES, COLS - WINDIV, 0, WINDIV);
+    }
+    wprintw(vizwin, " hup! ");
+    wrefresh(vizwin);
+    signal(sig, draws);
 }
