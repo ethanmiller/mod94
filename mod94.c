b88c13a003d52b56e581f7c8b3379d3153ef12b6
ethanmiller <thnntn@gmail.com>
2009-11-28 22:12:00 -0800

    got some shuffling words into arrays, and some strange behavior ive yet to figure out

--- mod94.c
+++ mod94.c
@@ -4,9 +4,16 @@
 
 #define WINDIV 30
 #define INTRVL 41666 // approx 24 times a second
+#define NFOUR 94
+#define WLEN 15
+
 
 void draws(int);
 int handle_char(char in_char);
+
+char chlist[NFOUR] = {'\0'};
+char whold[WLEN] = {'\0'};
+int poslist[NFOUR] = {0};
 
 int main(){
     char ch;
@@ -31,8 +38,38 @@ int setup(){
 
 int handle_char(char in_char){
     static WINDOW *charwin = NULL;
+    static int windx = 0;
+    static int clindx = 0;
+    int i, got_gap;
     if(charwin == NULL){
 	charwin = newwin(LINES, WINDIV, 0, 0);
+    }
+    
+    // add char to whold
+    whold[windx++] = in_char;
+    windx %= WLEN;
+    if(in_char == ' '){
+	//wprintw(charwin, "%d", clindx);
+	// copy word to chlist, and clear whold
+	for(i = 0; i < windx; i++){
+	    chlist[clindx++] = whold[i];
+	    clindx %= NFOUR;
+	    whold[i] = '\0';
+	}
+	// since we're prob overwriting prev words,
+	// clear till we get to a gap
+	got_gap = 0;
+	i = clindx;
+	while(!got_gap){
+	   if(chlist[i] == '\0' || chlist[i] == ' '){
+	       got_gap = 1;
+	   }else{
+	       chlist[i++] = ' ';
+	       i %= NFOUR;
+	   } 
+	}
+	// start new word in whold
+	windx = 0;
     }
     wprintw(charwin, "%c", in_char);
     wrefresh(charwin);
@@ -51,10 +88,24 @@ int start_anim(unsigned int seconds){
 
 void draws(int sig){
     static WINDOW *vizwin = NULL;
+    static int idx = 0;
+    int printed_space = 0;
     if(vizwin == NULL){
 	vizwin = newwin(LINES, COLS - WINDIV, 0, WINDIV);
     }
-    wprintw(vizwin, " hup! ");
-    wrefresh(vizwin);
+    // kind of wasting time here, but I want to make sure
+    // I'm cycling through arrays correctly
+
+    if(chlist[idx] != '\0'){
+	if(chlist[idx] == ' '){
+	    while(chlist[idx + 1] == ' '){
+		idx++;
+	    }
+	}
+	wprintw(vizwin, "%c", chlist[idx++]);
+	wrefresh(vizwin);
+    }else{
+	if(idx != 0) idx = 0;
+    }
     signal(sig, draws);
 }
